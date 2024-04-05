8e2d7d028bb48cd53cbcca99745ccc4da4afed2f
RED-Pixl <108759893+RED-Pixl@users.noreply.github.com>
2024-04-05 20:34:22 +0200

    added some text to website

--- src/App.vue
+++ src/App.vue
@@ -23,9 +23,9 @@
 
 <template>
         <div class="bg-fixed bg-cover min-h-screen width-full m-0 text-gray-100"
-                style="background-image: url('/src/assets/background.png')">
+                style="background-image: url('/src/assets/background.jpg')">
                 <div
-                        class="fixed my-6 p-4 left-[50%] translate-x-[-50%] max-xl:bottom-2 rounded-3xl border-y border-b-white/10 border-t-white/15 bg-black/15 shadow-xl shadow-black/30 backdrop-blur-3xl transition-transform hover:scale-110">
+                        class="fixed my-6 p-4 left-[50%] translate-x-[-50%] rounded-3xl border-y border-b-white/10 border-t-white/15 bg-black/15 shadow-xl shadow-black/30 backdrop-blur-3xl transition-transform hover:scale-110 z-50">
                         <div class="font-normal align-center flex flex-row justify-center">
                                 <button @click="page = 'home'" class="flex flex-row justify-center w-32 h-12 gap-2 rounded-xl p-2 transition hover:bg-white/5 active:scale-95 active:bg-white/10">
                                         <img src="/src/assets/icons/home.svg" class="w-6 h-6 my-auto" v-if="page === 'home'">
--- src/components/projects/Website.vue
+++ src/components/projects/Website.vue
@@ -2,7 +2,7 @@
 
 <template>
         <div
-                class="absolute m-6 p-4 rounded-full border-y border-b-white/10 border-t-white/15 bg-black/15 shadow-xl shadow-black/30 backdrop-blur-3xl transition-transform hover:scale-110">
+                class="fixed m-6 p-4 rounded-full border-y border-b-white/10 border-t-white/15 bg-black/15 shadow-xl shadow-black/30 backdrop-blur-3xl transition-transform hover:scale-110">
 
                 <div class="align-center flex flex-row justify-center">
                         <button @click="$emit('back')" class="flex flex-row w-12 h-12 gap-2 rounded-full p-2 transition hover:bg-white/5 active:scale-95 active:bg-white/10">
@@ -14,6 +14,16 @@
                 <h1 class="text-5xl text-center font-black flex flex-col w-max pt-40 mx-auto">
                         This Website
                 </h1>
+        </div>
+
+        <div class="flex flex-row justify-center">
+                <img src="/src/assets/website.png" class="w-96 h-96 m-16 justify-center items-center rounded-3xl bg-cover shadow-xl shadow-black/30 border-y border-b-white/10 border-t-white/15 transition-transform hover:scale-110"></img>
+
+                <div>
+                        <p class="m-12 w-5/6 max-w-7xl font-normal text-4xl drop-shadow-xl">
+                                Well, since you are already here, I don't need to talk much about the content and the visuals of this website.
+                        </p>
+                </div>
         </div>
 </template>
 
