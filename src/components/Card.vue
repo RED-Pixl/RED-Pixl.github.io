7c86fb32bc30f501ed540bf8d2980054bde5b30c
simon <simon.reilich@tum.de>
2024-04-11 15:00:20 +0200

    fixed error with lables in the project section

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,7 +7,7 @@
 <template>
         <div class="w-64 h-64 lg:w-96 lg:h-96 justify-center overflow-visible items-center rounded-3xl bg-cover shadow-xl shadow-black/30 transition-transform hover:scale-110 border-y border-b-white/10 border-t-white/15" :style="{backgroundImage: 'url(' + image + ')'}">
                 <div
-                        class="relative top-[14rem] lg:top-[21rem] w-30 lg:w-44 mx-auto p-4 rounded-3xl border-y border-b-white/10 border-t-white/15 bg-black/15 shadow-xl shadow-black/30 backdrop-blur-3xl tracking-normal">
+                        class="relative top-[14rem] lg:top-[21rem] w-44 mx-auto p-4 rounded-3xl border-y border-b-white/10 border-t-white/15 bg-black/15 shadow-xl shadow-black/30 backdrop-blur-3xl tracking-normal">
 
                         <div class="font-normal align-center flex flex-row justify-center">
                                 <button @click="$emit('show')" class="flex flex-row justify-center w-26 lg:w-40 h-12 gap-2 rounded-xl p-2 transition hover:bg-white/5 active:scale-95 active:bg-white/10">
